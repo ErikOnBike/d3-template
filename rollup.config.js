9c4b3688f9600128788abdb4d457e2e68d9b0e3c
Erik Stel <erik.stel@gmail.com>
2018-09-27 21:48:30 +0200

    Update packages and package dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,39 @@
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import { uglify } from "rollup-plugin-uglify";
 
-export default {
-  input: "index",
-  output: {
-    extend: true,
-    file: "build/d3-template.js",
-    format: "umd",
-    name: "d3",
-    globals: {
-      "d3-selection": "d3",
-      "d3-transition": "d3"
-    }
+export default [
+  {
+    input: "index",
+    output: {
+      extend: true,
+      file: "build/d3-template.js",
+      format: "umd",
+      name: "d3",
+      globals: {
+        "d3-selection": "d3",
+        "d3-transition": "d3"
+      }
+    },
+    external: [
+      "d3-selection",
+      "d3-transition"
+    ]
   },
-  plugins: [
-    resolve({
-      main: true,
-      jsnext: true,
-      browser: true
-    }),
-    commonjs()
-  ],
-  external: [
-    "d3-selection",
-    "d3-transition"
-  ]
-};
+  {
+    input: "index",
+    output: {
+      extend: true,
+      file: "build/d3-template.min.js",
+      format: "umd",
+      name: "d3",
+      globals: {
+        "d3-selection": "d3",
+        "d3-transition": "d3"
+      }
+    },
+    external: [
+      "d3-selection",
+      "d3-transition"
+    ],
+    plugins: [uglify()]
+  },
+];
